6c4983bd35db513c8f8493849aec2ec0748b96bf
Jamie Cook <jamie_cook@ajg.com>
2025-01-22 09:03:47 +0000

    Move to vim.opt for settings and add new

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,9 +1,22 @@
 -- Basic settings
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.termguicolors = true
-vim.o.wrap = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+
+vim.opt.cursorline = true
+
+vim.opt.clipboard:append("unnamedplus")
+
+vim.opt.inccommand = "split"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.swapfile = false
