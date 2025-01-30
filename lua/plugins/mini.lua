97c5a806a55741ce7df4744a214f42d85bdcf0ce
Jamie Cook <jamie@jaydubyasee.co.uk>
2025-01-30 20:29:45 +0000

    Replace mini files with oil

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,9 +4,6 @@ return {
   config = function()
     require('mini.ai').setup()
     require('mini.comment').setup()
-    require('mini.files').setup({
-
-    })
     require('mini.icons').setup()
     require('mini.pairs').setup()
     require('mini.pick').setup({
